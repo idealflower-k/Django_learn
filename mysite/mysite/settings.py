69f4779aa82364635c047276ab63080904b0683d
idealflower-k <aekrcu@naver.com>
2023-12-27 18:47:41 +0900

    feat: section 13-93 8min

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(+ocrl0k3!8p$tik9v%sk1i-y(5acvp=tefb48f#%_a6$_!+ia'
+SECRET_KEY = 'django-insecure-mh^a8t)xz$r)s4c!s7ec&&ydinw+_vmt4k)qd&2z5b0@vjf*0-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'cars.apps.CarsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
