8d366570b105219472d74e87949c6691361d1a97
idealflower-k <aekrcu@naver.com>
2024-01-04 17:38:27 +0900

    feat: p.160

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ExamplesConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'examples'
+    name = 'users'
